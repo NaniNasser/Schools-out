8fc9eba7f534fa28afe84827ac0d8ad35f787f5c
NaniNasser <Nasser.FAQIRI@student.intecbrussel.be>
2021-09-24 15:24:48 +0200

    added DAO

---
+++ src/main/java/schoolsout/daos/IDAO.java
@@ -0,0 +1,21 @@
+package schoolsout.daos;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public interface IDAO<T> {
+
+    default EntityManager getEntityManager(EntityManagerFactory emf){
+        return emf.createEntityManager();
+    }
+
+
+        void save(T t);
+        T findById(Object id);
+
+        List<T> findAll();
+        void update(T t);
+        void remove(T t);
+    }
+
+
---
+++ src/main/java/schoolsout/daos/PersonDAO.java
@@ -0,0 +1,63 @@
+package schoolsout.daos;
+
+import schoolsout.models.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class PersonDAO implements IDAO<Person> {
+
+    private EntityManagerFactory emf;
+
+    public PersonDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+
+    @Override
+    public void save(Person person) {
+        EntityManager em = getEntityManager(emf);
+        if (person.getId() == null) {
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+        }
+        em.close();
+    }
+
+    @Override
+    public Person findById(Object id) {
+        EntityManager em = getEntityManager(emf);
+        Person person = em.find(Person.class, id);
+        em.close();
+        return person;
+    }
+
+    @Override
+    public List<Person> findAll(Person person) {
+        EntityManager em = getEntityManager(emf);
+        if (person.getId() == ) {
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+        }
+
+
+    @Override
+    public void update(Person person) {
+        EntityManager em = getEntityManager(emf);
+        if (person.getId() != null){
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+        }
+        em.close();
+    }
+
+
+    @Override
+    public void remove(Person person) {
+
+    }
+}
