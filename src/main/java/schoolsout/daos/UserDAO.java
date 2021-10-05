d75853a2e6483d0bfa93d8b142b409c57e3b1781
NaniNasser <Nasser.FAQIRI@student.intecbrussel.be>
2021-10-05 13:37:25 +0200

    update UserDAO

--- src/main/java/schoolsout/daos/UserDAO.java
+++ src/main/java/schoolsout/daos/UserDAO.java
@@ -19,32 +19,39 @@ public class UserDAO implements IDAO<User>{
 
     @Override
     public void save(User user) {
-        EntityManager em = getEntityManager(emf);
-        {
+        if (findById(user.getLogin()) == null) {
+            EntityManager em = getEntityManager(emf);
+
             em.getTransaction().begin();
             em.persist(user);
             em.getTransaction().commit();
+
+            em.close();
         }
-        em.close();
+       else System.out.println("User does not exist");
     }
 
+
     public User smartSave(User user) {
-        EntityManager em = getEntityManager(emf);
-        em.getTransaction().begin();
-        em.persist(user);
-        em.flush();
-        em.getTransaction().commit();
-        em.close();
-        return user;
+        if (findById(user.getLogin()) == null) {
+            EntityManager em = getEntityManager(emf);
+            em.getTransaction().begin();
+            em.persist(user);
+            em.flush();
+            em.getTransaction().commit();
+            em.close();
+            return user;
+        }
     }
 
     @Override
     public User findById(Object id) {
-        EntityManager em = getEntityManager(emf);
-        User user = em.find(User.class, id);
-        em.close();
-        return user;
-    }
+            EntityManager em = getEntityManager(emf);
+            User user = em.find(User.class, id);
+            em.close();
+            return user;
+        }
+
 
     @Override
     public List<User> findAll() {
@@ -57,25 +64,29 @@ public class UserDAO implements IDAO<User>{
 
     @Override
     public void update(User user) {
-        EntityManager em = getEntityManager(emf);
-       {
+        if (findById(user.getLogin()) != null){
+            EntityManager em = getEntityManager(emf);
+
             em.getTransaction().begin();
             em.merge(user);
             em.getTransaction().commit();
+
+            em.close();
         }
-        em.close();
     }
 
     @Override
     public void remove(User user) {
-        EntityManager em = getEntityManager(emf);
-        {
+        if (findById(user.getLogin()) != null) {
+            EntityManager em = getEntityManager(emf);
+
             em.getTransaction().begin();
             em.remove(em.contains(user) ? user : em.merge(user));
             em.flush();
             em.getTransaction().commit();
-        }
+
         em.close();
+        }
     }
 }
 
